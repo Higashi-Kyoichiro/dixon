6c213ed45e4bbc47753beeb38b72bd287e539144
Higashi-Kyoichiro <75964531+Higashi-Kyoichiro@users.noreply.github.com>
2021-01-30 11:20:33 +0900

    Update dixon.py

--- dixon.py
+++ dixon.py
@@ -11,49 +11,64 @@ This program was written by Kyoichiro Higashi PHD. in Meiji Pharmaceutical Unive
 '''
 import numpy as np
 
-def competitive(x, Vmax=1, Km=1, Ki=1):
+def make_exog(S,I):
+	'''
+	arguments:
+		S:Substrate in list
+		I:Inhibitor(or activator) in list
+	return:
+		array formatted Substrates-series and inhibitor series
+	'''
+	return np.array([S*len(I), np.array([[i]*len(S) for i in I]).flatten()])
+
+def competitive(exog, Vmax=1, Km=1, Ki=1):
 	'''
 	competitive inhibition kinetics
 	arguments:
-	x (array): array([Substrate,Inhibitor])
+	exog (array): [Substrate,Inhibitor]
 	Vmax=1, Km=1, Ki=1
 	returns: initial rates'''
-	S, I = x
+	S, I = exog
 	v = Vmax * S / (Km * (1 + I/Ki) + S) 
 	return v
 
-def noncompetitive(x, Vmax=1, Km=1, Ki=1, alpha=0):
+def noncompetitive(exog, Vmax=1, Km=1, Ki=1, alpha=0):
 	''' 
 	noncompetitive inhibition kinetics.
 	This function treats partial inhibition as well as complete inhibition.
 	arguments:
-	x (array): array([Substrate,Inhibitor])
+	exog (array): [Substrate,Inhibitor]
 	Vmax=1, Km=1, Ki=1, alpha=0
 	alpha means ratio of Vmax with maximal inhibitor to Vmax without inhibitor.
 	Alpha=0 means Complete inhibition. 
 	returns: initial rates'''
-	S,I = x
+	S, I = exog
 	v = Vmax*S/(Km+S)*Ki/(Ki+I) + alpha*Vmax*S/(Km+S)*I/(Ki+I) 
 	return v
 
-def anticompetitive(x, Vmax=1, Km=1, Ki=1,alpha=0):
+def anticompetitive(exog, Vmax=1, Km=1, Ki=1,alpha=0):
 	'''
 	anticompetitive (also known as uncompetitive) inhibition kinetics.
 	arguments:
-	x (array): array([Substrate,Inhibitor])
+	exog (array): [Substrate,Inhibitor]
 	Vmax=1, Km=1, Ki=1, alpha=0
 	Alpha=0 means Complete inhibition. 
 	returns: initial rates'''
-	S, I = x
+	S, I = exog
 	v = Vmax * S / (Km + S * (1 + I / Ki)) + alpha * I/Ki * Vmax * S / (Km + S * (1 + I/Ki))
 	return v
 
-def mixed(x, Vmax=1, Km=1, Ki=1, alpha=0.3, beta=3):
+def mixed(exog, Vmax=1, Km=1, Ki=1, alpha=0.3, beta=3):
 	'''
 	Mixed type inhibition means random-order binding of inhibitor and substrate. Alpha means the degree of decrease in Vmax, 
 	beta means increase in Km.
-	'''
-	S, I = x[0], x[1]
+	exog (array): [Substrate,Inhibitor]
+	Vmax=1, Km=1, Ki=1,alpha=0.3,beta=3
+	alpha means ratio of inhibited Vmax with maximal inhibitor to Vmax without inhibitor.
+	Alpha=0.3 means partial inhibition.
+	beta means ratio of Km with inhibitor to Km without inhibitor.
+	returns: initial rates	'''
+	S, I = exog
 	v = Vmax * S / (S + Km * (1 + I/Ki * (1 + S/(Km * beta)))) + \
 	Vmax * alpha * S * I / (Ki * beta) / (S + Km * (1 + I/Ki * (1 + S/(Km * beta)))) 
 	return v
